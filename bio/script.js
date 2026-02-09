7e7f0f637cc440b294feaa6c106a5d6045a40f2c
Sebastião Lustosa Frota Júnior <sebastiaolustosafrotajunior@Laptop-de-Sebastiao.local>
2026-02-09 17:17:06 -0300

    Bio Vanessa Hair - versao inicial

---
+++ bio/script.js
@@ -0,0 +1,16 @@
+/**
+ * Vanessa Hair - Bio
+ */
+
+document.addEventListener('DOMContentLoaded', () => {
+  initYear();
+});
+
+/* ==========================================
+   UTILS
+   ========================================== */
+
+function initYear() {
+  const el = document.getElementById('year');
+  if (el) el.textContent = new Date().getFullYear();
+}
